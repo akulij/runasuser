ba074b4ce0e6c7b2d8caad8350b736f3321b4080
akulij <48361256+akulij@users.noreply.github.com>
2023-01-05 16:36:41 +0500

    creating error message generator

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,16 @@ use winapi::um::winnt::HANDLE;
 mod tools;
 
 
+fn format_error_message(error_code: u32) -> String {
+    format!("
+Can't create process due {0} error code.
+Read page https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes#system-error-codes
+Невозможно создать процесс из-за ошибки с кодом {0}.
+Читайте страницу https://learn.microsoft.com/ru-ru/windows/win32/debug/system-error-codes#system-error-codes
+", error_code)
+
+}
+
 fn service_token_by_login(login: &str, password: &str) -> Result<HANDLE, (String, u32)> {
     let login_raw = tools::encode_str(login);
     let password_raw = tools::encode_str(password);
