e473a2b3580a7ddd6f33ed3dd2e50179eca403c1
akulij <48361256+akulij@users.noreply.github.com>
2023-01-05 19:25:30 +0500

    lint tools module

--- src/tools.rs
+++ src/tools.rs
@@ -3,7 +3,10 @@ pub fn encode_str(s: &str) -> *mut u16 {
 }
 
 pub fn generate_cmd(args: &Vec<&str>) -> *mut u16 {
-    let mut cmd_str: Vec<u16> = args.join(" ").as_str().encode_utf16().collect();
+    let mut cmd_str: Vec<u16> = args.join(" ")
+                                    .as_str()
+                                    .encode_utf16()
+                                    .collect();
     cmd_str.push(0);
 
     cmd_str.as_mut_ptr()
