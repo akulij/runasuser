61afa26cba9df0c5228c00e9ad8d709677b901cc
akulij <48361256+akulij@users.noreply.github.com>
2023-01-05 16:29:17 +0500

    create command line generation utility function

--- src/tools.rs
+++ src/tools.rs
@@ -1,3 +1,10 @@
 pub fn encode_str(s: &str) -> *mut u16 {
     s.encode_utf16().collect::<Vec<u16>>().as_mut_ptr()
 }
+
+pub fn generate_cmd(args: &Vec<&str>) -> *mut u16 {
+    let mut cmd_str: Vec<u16> = args.join(" ").as_str().encode_utf16().collect();
+    cmd_str.push(0);
+
+    cmd_str.as_mut_ptr()
+}
